076f8163e5b8ef7f3f88ec96c63645e310fc0a6f
Nikolaj Hjelm Kaplan <nikolaj.kaplan@kraftvaerk.com>
2017-12-19 10:43:55 +0100

    WIP

--- OKQ8.ServiceLayer/ICard.cs
+++ OKQ8.ServiceLayer/ICard.cs
@@ -13,5 +13,17 @@ namespace OKQ8.ServiceLayer
         getNumberOfCardsByTypeResponse1 GetNumberOfCardsByType(string token);
 
 
+        getTransactionsResponse1 GetCardTransactions(string token, string[] cardNumbers, TimePeriod period, invoiced invoiced);
+        getTransactionDetailsResponse1 GetTransactionDetails(string token, string accountNumber, string bonNumber, DateTime transactionDate, string cardNumber);
+        orderCardResponse1 OrderCard(string token, string accountId, CardType cardType, string cardName, string email, string messageToQ8, string language, string ipAddress);
+        changePINCodeResponse1 ChangePinCode(string token, string cardNumber, string oldPinCode, string newPinCode, string ipAddress);
+        closeCardResponse1 CloseCard(string token, string cardNumber, string ipAddress);
+        updateInfoLineQ8CardResponse1 UpdateInfoLineQ8Card(string token, string cardNumber, string cardInfoText);
+
+        getLiterCountResponse1 GetLiterCount(string token, string[] cardNumber, DateTime fromDate, DateTime toDate);
+        getCountOfCardsForSmlAccountResponse1 GetCountOfCardsForSmlAccount(string token);
+        getStationDataResponse1 GetStationData(string bKontoNr);
+
+
     }
 }
--- OKQ8.ServiceLayer/IInvoice.cs
+++ OKQ8.ServiceLayer/IInvoice.cs
@@ -6,33 +6,31 @@ namespace OKQ8.ServiceLayer
     public interface IInvoice
     {
         // Invoice App
-        getLiterCountResponse1 GetLiterCount(string token, string[] cardNumber, DateTime fromDate, DateTime toDate);
-
-
-
-        getTransactionsResponse1 GetCardTransactions(string token, string[] cardNumbers, TimePeriod period, invoiced invoiced);
-        getTransactionDetailsResponse1 GetTransactionDetails(string token, string accountNumber, string bonNumber, DateTime transactionDate, string cardNumber);
-        getLastInvoiceResponse1 GetLastInvoiceTransactions(string token, string accountNumber);
-        orderCardResponse1 OrderCard(string token, string accountId, CardType cardType, string cardName, string email, string messageToQ8, string language, string ipAddress);
-        changePINCodeResponse1 ChangePinCode(string token, string cardNumber, string oldPinCode, string newPinCode, string ipAddress);
-        closeCardResponse1 CloseCard(string token, string cardNumber, string ipAddress);
-        updateInfoLineQ8CardResponse1 UpdateInfoLineQ8Card(string token, string cardNumber, string cardInfoText);
 
         getDocumentResponse1 GetDocument(string token, string tsqname, string cicsid);
         List<Doc2ArchiveSearchResultRow> GetArchiveFromList(string token, IEnumerable<string> accountNumbers, TimePeriod period, ArchiveType invoice);
+        getLastInvoiceResponse1 GetLastInvoiceTransactions(string token, string accountNumber);
 
-        getAuditResponse1 GetAudit(string token, TimePeriod period, string operation, string @object, string user, int maxcount, int samAccount);
-        getSamAccountsResponse1 GetSamAccounts(string token);
-        getCurrentSamAccountResponse1 GetCurrentSamAccount(string token);
-        loadFromAccountSmlResponse1 LoadFromAccount(string token, string saccount);
-        sendSMSResponse1 SendSms(string q8UserName, string q8Password, string to, string type, string from, string text);
-        getHAccountsResponse1 GetHAccounts(string token, allowAccountTypes type);
-        getUAccountsResponse1 GetUAccounts(string token);
-        getAccessResponse1 GetAccess(string token);
-        getCountOfCardsForSmlAccountResponse1 GetCountOfCardsForSmlAccount(string token);
-        getStationDataResponse1 GetStationData(string bKontoNr);
-
-        wsSuccess CreateCustomerServiceTask(string token, q8KCTasks q8KCTasks, string custNo, string email, string text);
+        /// TODO Lasse:
+        /// we need a list of invoices with an id for each invoice.
+        /// This id can be used in two services that can 
+        /// either get all transactions included in the invoice 
+        /// or get the invoice as a pdf (byte stream)
 
     }
+
+    public interface IGeneral
+    {
+        sendSMSResponse1 SendSms(string q8UserName, string q8Password, string to, string type, string from, string text);
+        wsSuccess CreateCustomerServiceTask(string token, q8KCTasks q8KCTasks, string custNo, string email, string text);
+        getAuditResponse1 GetAudit(string token, TimePeriod period, string operation, string @object, string user, int maxcount, int samAccount);
+    }
+
+    public interface IAccount
+    {
+        getHAccountsResponse1 GetHAccounts(string token, allowAccountTypes type);
+        getUAccountsResponse1 GetUAccounts(string token);
+    }
+
+
 }
